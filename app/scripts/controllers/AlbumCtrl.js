2c2d6ecd19721c8759b4c08d5ad8f44828532d7b
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-08-22 15:38:28 -0400

    Checkpoint 7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,10 @@
-(function(){
-    function AlbumCtrl(Fixtures){
-      this.albumData= Fixtures.getAlbum();
-}
+(function() {
+    function AlbumCtrl(Fixtures, SongPlayer) {
+        this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
+    }
 
-  angular
-      .module('blocJams')
-      .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
