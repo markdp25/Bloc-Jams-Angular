5e0767e6bf3f6efdae30ceb8e5f43a6921732c35
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-08-22 15:46:31 -0400

    Assignment checkpoint 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,7 +2,10 @@
     function SongPlayer() {
         var SongPlayer = {};
 
+//        @desc current song object set to null when page first loads
+//        @type {Object}
         var currentSong = null;
+
 //        @desc Buzz object audio file
 //        @type {Object}
         var currentBuzzObject = null;
@@ -24,22 +27,42 @@
             currentSong = song;
         };
 
+//        @function playSong
+//        @desc plays song and sets song.playing to true so album.html changes play/pause icon
+//        @param {Object} song
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+
+//        @function pauseSong
+//        @desc pauses song and sets song.playing to false so album.html changes play/pause icon
+//        @param {Object} song
+        var pauseSong = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+
+//        @function SongPlayer.play(song)
+//        @desc plays a song from the beginning if the song has not already started and continues playing the song from where it left off if not
+//        @params {Object} song
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
 
+//        @function SongPlayer.pause(song)
+//        @desc pauses a song at its current time point
+//        @params {Object} song
         SongPlayer.pause = function(song) {
-            currentBuzzObject.pause();
-            song.playing = false;
+            pauseSong(song);
         };
 
         return SongPlayer;
