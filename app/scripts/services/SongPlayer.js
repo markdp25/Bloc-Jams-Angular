ec3a8233abd4a1909e59906cf32936e742cf36ec
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-08-22 18:42:00 -0400

    Checkpoint 8 Coursework

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,9 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
-
-//        @desc current song object set to null when page first loads
-//        @type {Object}
-        var currentSong = null;
-
+        // @desc which album is playing
+        // @type {object}
+        var currentAlbum = Fixtures.getAlbum();
 //        @desc Buzz object audio file
 //        @type {Object}
         var currentBuzzObject = null;
@@ -16,7 +14,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -24,7 +22,7 @@
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
 
 //        @function playSong
@@ -33,7 +31,7 @@
         var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
-        }
+        };
 
 //        @function pauseSong
 //        @desc pauses song and sets song.playing to false so album.html changes play/pause icon
@@ -41,17 +39,28 @@
         var pauseSong = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
-        }
+        };
+//      @function getSongIndex
+//      @desc gets the song index for use in next/previous buttons
+//      @param {object} song
+        var getSongIndex = function(song){
+            return currentAlbum.songs.indexOf(song);
+        };
+
+//     @desc Active song object from list of songs
+//     @type {Object}
+        SongPlayer.currentSong = null;
 
 //        @function SongPlayer.play(song)
 //        @desc plays a song from the beginning if the song has not already started and continues playing the song from where it left off if not
 //        @params {Object} song
         SongPlayer.play = function(song) {
-            if (currentSong !== song) {
+          song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
 
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     playSong(song);
                 }
@@ -62,13 +71,29 @@
 //        @desc pauses a song at its current time point
 //        @params {Object} song
         SongPlayer.pause = function(song) {
+          song = song || SongPlayer.currentSong;
             pauseSong(song);
         };
+//        @function SongPlayer.previous
+//        @desc skips backward to previous song, stops playing music if song #1
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+                currentSongIndex--;
+
+                if (currentSongIndex < 0) {
+                    currentBuzzObject.stop();
+                    SongPlayer.currentSong.playing = null;
+                } else {
+                    var song = currentAlbum.songs[currentSongIndex];
+                    setSong(song);
+                    playSong(song);
+                }
+         };
 
         return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
